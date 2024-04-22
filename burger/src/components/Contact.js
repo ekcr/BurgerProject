118481476399d97298b080f74b5dfa3d6bdc7a03
enes.kacar <enes.kacar@etiya.com>
2024-04-22 21:44:26 +0300

    about & contact page development.

--- burger/src/components/Contact.js
+++ burger/src/components/Contact.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import contactImage from '../assets/five2.jpeg'
+import '../styles/Contact.css'
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className='contact-image' style={{backgroundImage: `url(${contactImage})`}}></div>
+      <div className='contact-us'>
+        <h1> Bizimle İletişime Geçin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input
+            type='text'
+            name='name'
+            placeholder='Lütfen doldurunuz...'
+          />
+          <label>Mail</label>
+          <input
+            type='mail'
+            name='mail'
+            placeholder='Lütfen doldurunuz...'
+          />
+          <label>Mesaj</label>
+          <textarea
+            name='message'
+            placeholder='Lütfen doldurunuz...'
+          />
+          <button> Gönder </button>
+        </form>
+      </div>
+    </div>
   )
 }
